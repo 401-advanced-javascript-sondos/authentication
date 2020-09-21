1c6b936cfd8d66b312151fb5e6f3f4a2b79830f8
SondosMatahen <sondosmatahen@gmail.com>
2020-09-21 14:40:29 +0300

    edit code and test

--- src/auth/models/usersModel.js
+++ src/auth/models/usersModel.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const bcrypt=require('bcrypt');
 const jwt = require('jsonwebtoken');
-const uniqueValidator = require('mongoose-unique-validator');
+// const uniqueValidator = require('mongoose-unique-validator');
 
 
 
@@ -15,7 +15,7 @@ let User = mongoose.Schema({
   // role: { type: string, enum: ['admin', 'editor', 'writer', 'user'] }
 });
 
-User.plugin(uniqueValidator);
+// User.plugin(uniqueValidator);
 
 
 User.pre('save', async function(next){
@@ -26,23 +26,32 @@ User.pre('save', async function(next){
 
 
 
-User.methods.authenticate = async function(){
-  let {username , password}=this;
+User.statics.authenticate = async function(username , password){
+  //username and password come from new schema from basic auth line 19
+  // password here is not bcrypt
+  // let {username , password}=this;
   // console.log('this',this);
-  let user= await this.constructor.findOne({username});
-  // console.log('user',user);
+  let user= await this.findOne({username});
+  console.log('user',user);
   let valid= await bcrypt.compare(password,user.password);
   // console.log('valid',valid);
   return {valid,user};
 };
 
 
-User.methods.generateToken=function(){
-  const { username } = this;
-  const token=jwt.sign({username},process.env.KEY);
+// User.methods.generateToken=function(){
+//   const { username } = this;
+//   const token=jwt.sign({username},process.env.KEY);
+//   // console.log('token',token);
+//   return token;
+// };
+
+
+User.statics.generateToken=function(user){
+  // const { username } = this;
+  const token=jwt.sign({username:user.username},process.env.KEY);
   // console.log('token',token);
   return token;
 };
-
 
 module.exports =mongoose.model('User',User);
